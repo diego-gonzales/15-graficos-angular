6d1fce41fef128424c5592e1392252fb8dd82802
Diego Gonzales <dgonzalesj14@unc.edu.pe>
2020-12-16 16:26:30 -0500

    se agrego el barChart funciona bien

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LineComponent } from './line/line.component';
 import { ChartsModule } from 'ng2-charts';
+import { BarComponent } from './bar/bar.component';
 
 
 
 @NgModule({
   declarations: [
-    LineComponent
+    LineComponent,
+    BarComponent
   ],
   imports: [
     CommonModule,
     ChartsModule
   ],
   exports: [
-    LineComponent
+    LineComponent,
+    BarComponent
   ]
 })
 export class ComponentsModule { }
