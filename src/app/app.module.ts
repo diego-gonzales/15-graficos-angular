f7e4f1920e8a5dcf13d57fe9383de43960d824ba
Diego Gonzales <dgonzalesj14@unc.edu.pe>
2020-12-16 15:18:05 -0500

    grafico de lineas de ChartJS terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LineComponent } from './line/line.component';
+import { ChartsModule } from 'ng2-charts';
+
+
+
+@NgModule({
+  declarations: [
+    LineComponent
+  ],
+  imports: [
+    CommonModule,
+    ChartsModule
+  ],
+  exports: [
+    LineComponent
+  ]
+})
+export class ComponentsModule { }
